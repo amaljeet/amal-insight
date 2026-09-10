ed011bacb717a1448d7488ba7c99ef1ad8ab9c83
amaljeet <Amaljeet.Puthiya@insight.com>
2026-09-11 05:10:51 +0530

    Configure Azure backend for Terraform state management
    Added Terraform backend configuration for Azure storage.

--- main.tf
+++ main.tf
@@ -5,12 +5,22 @@ terraform {
       version = "5.4.0"
     }
   }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "bala_rg"
+    storage_account_name = "balastorage1"
+    container_name       = "balacontainer"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
 }
 provider "azurerm" {
   features {
   }
 }
 resource "azurerm_resource_group" "test" {
-name = "amal-pord-01"
+name = "amal-pord-02"
 location = "centralindia"
 }
